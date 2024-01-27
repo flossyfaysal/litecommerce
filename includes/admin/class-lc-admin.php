6617dd4babab8376d9f47d23e1f080efd34765f3
flossyfaysal <skfaysalalom@gmail.com>
2024-01-27 23:49:01 +0600

    admin class constructed

--- includes/admin/class-lc-admin.php
+++ includes/admin/class-lc-admin.php
@@ -3,11 +3,39 @@
 class LC_Admin
 {
 
-    protected $id;
-    protected $label;
     public function __construct()
     {
+        add_action('init', array($this, 'includes'));
+        add_action('current_screen', array($this, 'conditional_includes'));
+        add_action('admin_init', array($this, 'buffer'), 1);
+        add_action('admin_init', array($this, 'preview_emails'));
+        add_action('admin_init', array($this, 'prevent_admin_access'));
+        add_action('admin_init', array($this, 'admin_redirects'));
+        add_action('admin_footer', 'lc_print_js', 25);
+        add_filter('admin_footer_text', array($this, 'admin_footer_text'), 1);
+        add_filter('action_scheduler_post_type_args', array($this, 'disable_webhook_post_export'));
+        add_filter('admin_body_class', array($this, 'include_admin_body_class'), 9999);
 
+    }
+
+    public function preview_emails()
+    {
+
+    }
+
+    public function includes()
+    {
+
+    }
+
+    public function conditional_includes()
+    {
+
+    }
+
+    public function buffer()
+    {
+        ob_start();
     }
 }
 
