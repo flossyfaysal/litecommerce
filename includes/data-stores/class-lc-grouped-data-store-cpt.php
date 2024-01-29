eb989c03c906a813d21ae41419f51dff4e270c01
flossyfaysal <skfaysalalom@gmail.com>
2024-01-29 15:54:00 +0600

    settings api done

---
+++ includes/data-stores/class-lc-grouped-data-store-cpt.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class LC_Product_Grouped_Data_Store_CPT file.
+ * 
+ * @package Litecommerce\DataStores
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class LC_Product_Grouped_Data_Store_CPT extends LC_Product_Data_Store_CPT implements LC_Object_Data_Store_Interface
+{
+    protected function update_post_meta(&$product, $force = false)
+    {
+        $meta_key_to_props = array(
+            '_children' => 'children'
+        );
+        $props_to_update = $force ? $meta_key_to_props : $this->get_props_to_update($product, $meta_key_to_props);
+
+        foreach ($props_to_update as $meta_key => $prop) {
+            $value = $product->{"get_$prop"}('edit');
+            $updated = update_post_meta($product->get_id(), $meta_key, $value);
+            if ($updated) {
+                $this->updated_props[] = $prop;
+            }
+        }
+
+        parent::update_post_meta($product, $force);
+    }
+}
